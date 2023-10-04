daebc2c0c25f9aa9d9894b4df9becd3aac612578
KishoreMSK <mkishore2610@gmail.com>
2023-10-04 22:39:36 +0530

    added key prop

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           handleSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
